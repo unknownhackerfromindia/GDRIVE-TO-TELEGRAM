2ca3aa3a64be7a0703f73483296433f40cade6cc
MiyukiKun <miyukibot1@gmail.com>
2021-12-04 01:23:59 +0530

    V1

---
+++ downloader.py
@@ -0,0 +1,56 @@
+import os
+import requests
+import time
+
+class Timer:
+    def __init__(self, time_between=5):
+        self.start_time = time.time()
+        self.time_between = time_between
+
+    def can_send(self):
+        if time.time() > (self.start_time + self.time_between):
+            self.start_time = time.time()
+            return True
+        return False
+
+def human_readable_size(size, decimal_places=2):
+    for unit in ['B', 'KB', 'MB', 'GB', 'TB', 'PB']:
+        if size < 1024.0 or unit == 'PB':
+            break
+        size /= 1024.0
+    return f"{size:.{decimal_places}f} {unit}"
+
+def progress_bar_str(done, total):
+    percent = round(done/total*100, 2)
+    strin = "░░░░░░░░░░"
+    strin = list(strin)
+    for i in range(round(percent)//10):
+        strin[i] = "█"
+    strin = "".join(strin)
+    final = f"Percent: {percent}%\n{human_readable_size(done)}/{human_readable_size(total)}\n{strin}"
+    return final
+
+
+async def DownLoadFile(url, reply, chunk_size=1024*10, file_name="file.mkv"):
+    if os.path.exists(file_name):
+        os.remove(file_name)
+    if not url:
+        return file_name
+    timer = Timer()
+
+    r = requests.get(url, allow_redirects=True, stream=True)
+    total_size = int(r.headers.get("content-length", 0))
+    downloaded_size = 0
+    with open(file_name, 'wb') as fd:
+        for chunk in r.iter_content(chunk_size=chunk_size):
+            if chunk:
+                fd.write(chunk)
+                downloaded_size += chunk_size
+                if timer.can_send():
+                    try:
+                        data = progress_bar_str(downloaded_size, total_size)
+                        await reply.edit(f"Downloading\n{data}")
+                    except:
+                        pass
+
+    return file_name
---
+++ main.py
@@ -0,0 +1,63 @@
+from telethon import events
+from config import bot, API_KEY, FOLDER_URL, FILE_URL
+from FastTelethonhelper import fast_upload
+import requests
+import os
+from downloader import DownLoadFile
+
+
+@bot.on(events.NewMessage(pattern=(f"/start")))
+async def start(event):
+    await bot.send_message(event.chat_id, "Im Running")
+
+
+@bot.on(events.NewMessage(pattern=f"/download"))
+async def _(event):
+    data = event.text.split(":")
+    if "folder" in data[1]:
+        folder_id = data[1].split("/")[-1]
+        url = FOLDER_URL
+        url = url.replace("[FOLDER_ID]", folder_id)
+        r = requests.get(f"{url}{API_KEY}")
+        j = r.json()
+        main = await event.reply("STATUS:")
+        items = j['items'][::-1]
+        for i in items:
+            name = i['title']
+            file_id = i['id']
+            url = FILE_URL
+            url = url.replace("[FILE_ID]", file_id)
+            url = f"{url}{API_KEY}"
+            reply = await event.reply("Downloading...")
+            await main.edit(f"STATUS:\n`Downloading {name}`")
+            f = await DownLoadFile(url, reply, file_name=name)
+            await main.edit(f"STATUS:\n`Uploading {name}`")
+            file = await fast_upload(bot, f, reply)
+            await bot.send_message(event.chat_id, f, file=file, force_document= True)
+            await reply.delete()
+            os.remove(f)
+        await main.edit("ALL FILES UPLOADED")
+
+    elif "file" in data[1]:
+        file_id = data[1].split("/")[-1]
+        url = FILE_URL
+        url = url.replace("[FILE_ID]", file_id)
+        url = f"{url}{API_KEY}"
+        info_url = FILE_URL
+        info_url = info_url.replace("[FILE_ID]", file_id)
+        info_url = info_url.replace("alt=media&", "")
+        r = requests.get(f"{info_url}{API_KEY}")
+        j = r.json()
+        name = j['name']
+        
+        reply = await event.reply("Downloading...")
+        f = await DownLoadFile(url, reply, file_name=name)
+        file = await fast_upload(bot, f, reply)
+        await bot.send_message(event.chat_id, f, file=file, force_document= True)
+        await reply.delete()
+        os.remove(f)
+
+
+bot.start()
+
+bot.run_until_disconnected()
