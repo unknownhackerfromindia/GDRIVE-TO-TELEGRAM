7d6894f87cadb67fc49579d1894caf4f275c0cfa
MiyukiKun <miyukibot1@gmail.com>
2021-12-04 01:27:20 +0530

    V1

--- config.py
+++ config.py
@@ -4,8 +4,7 @@ from telethon import TelegramClient
 api_id = os.environ.get('API_ID')
 api_hash = os.environ.get('API_HASH')
 bot_token = os.environ.get('BOT_TOKEN')
-# bot_username = os.environ.get('BOT_USERNAME')
-API_KEY = 'AIzaSyC--9GmvcH5LD9ACAqJ_fHrEbfUH8A3-vU'
+API_KEY = os.environ.get('API_KEY')
 FOLDER_URL = "https://www.googleapis.com/drive/v2/files?q='[FOLDER_ID]'+in+parents&key="
 FILE_URL = "https://www.googleapis.com/drive/v3/files/[FILE_ID]?alt=media&key="
 
