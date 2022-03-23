6f35d1bec2f938ab2058e8423091c030b6f75a46
ADITYA <83854084+adityasirohi4@users.noreply.github.com>
2022-03-23 20:50:49 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,23 +4,8 @@ from telethon import TelegramClient
 api_id = os.environ.get('API_ID')
 api_hash = os.environ.get('API_HASH')
 bot_token = os.environ.get('BOT_TOKEN')
-bot_username = os.environ.get('BOT_USERNAME')
 API_KEY = os.environ.get('API_KEY')
 FOLDER_URL = "https://www.googleapis.com/drive/v2/files?q='[FOLDER_ID]'+in+parents&key="
 FILE_URL = "https://www.googleapis.com/drive/v3/files/[FILE_ID]?alt=media&key="
-BASE = "-1001598582374"
-FFMPEG = "-1001598582374"
-FFMPEGID = "2 3 4"
-FFMPEGCMD = "5"
-DESTINATION = "-1001598582374"
-
-FFMPEGID = FFMPEGID.split()
-for i in range(len(FFMPEGID)):
-    FFMPEGID[i] = int(FFMPEGID[i])
-
-BASE = int(BASE)
-FFMPEG = int(FFMPEG)
-FFMPEGCMD = int(FFMPEGCMD)
-DESTINATION = int(DESTINATION)
 
 bot = TelegramClient('bot', api_id, api_hash).start(bot_token=bot_token)
